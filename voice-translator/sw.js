01bd30cbf42156ab7221b9361246a134496615b2
MarSely-GH <mihail.martynov.2011@gmail.com>
2026-08-30 21:38:15 +0500

    Bump translator cache for smart punctuation

--- voice-translator/sw.js
+++ voice-translator/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'voice-translator-v6';
+const CACHE_NAME = 'voice-translator-v7';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './voice-gender.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
