4c30ebbe7d48ac3db7db95ee23bacbcd321d2d3f
MarSely-GH <mihail.martynov.2011@gmail.com>
2026-08-30 21:29:32 +0500

    Bump translator cache to version 6

--- voice-translator/sw.js
+++ voice-translator/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'voice-translator-v5';
+const CACHE_NAME = 'voice-translator-v6';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './voice-gender.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
